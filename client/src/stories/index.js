08f75ee1faa155679c6d1de617408b51bbd9af3e
Björn Lárus <bjornlarus@gmail.com>
2018-03-17 11:12:55 +0000

    colors storybook

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -7,6 +7,7 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 
 import Logo from '../assets/img/logo.svg';
+import { color } from '../styles/color';
 
 storiesOf('Welcome', module).add('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
@@ -20,4 +21,53 @@ storiesOf('Button', module)
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
 
-storiesOf('Logo', module).add('Myshopover Logo', () => <img src={Logo} />);
+storiesOf('Brand', module)
+  .add('Logo', () => <img src={Logo} />)
+  .add('Colors', () => (
+    <div style={{ display: 'flex' }}>
+      <div>
+        <div
+          style={{
+            background: color.primary,
+            height: '150px',
+            width: '150px',
+            marginRight: '20px'
+          }}
+        />
+        <span>#03CEA4</span>
+      </div>
+      <div>
+        <div
+          style={{
+            background: color.secondary,
+            height: '150px',
+            width: '150px',
+            marginRight: '20px'
+          }}
+        />
+        <span>#970747</span>
+      </div>
+      <div>
+        <div
+          style={{
+            background: color.lightgrey,
+            height: '150px',
+            width: '150px',
+            marginRight: '20px'
+          }}
+        />
+        <span>#F2F2F2</span>
+      </div>
+      <div>
+        <div
+          style={{
+            background: color.dark,
+            height: '150px',
+            width: '150px',
+            marginRight: '20px'
+          }}
+        />
+        <span>#000</span>
+      </div>
+    </div>
+  ));
